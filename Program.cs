96f90190739a4e97fecaa7bd6e09e4a350ef8654
Dell-User <Dell-User@DESKTOP-8EU8MHV>
2023-10-09 10:44:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    /|");
+            Console.WriteLine("   / |");
+            Console.WriteLine("  /  |");
+            Console.WriteLine(" /   |");
+            Console.WriteLine("/    |");
+
+            //to pause the console else it closes on its own!
+            Console.ReadLine();
+        }   
+    }
+}
